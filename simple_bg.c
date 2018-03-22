c14d0f20a2294db2b755827cb7c963831e5e9185
transistorretorcido <santiago@debianStretch>
2018-03-22 19:17:06 -0300

    new file:   Makefile 	new file:   assets/16x16_pixel_df.png 	new file:   assets/birdy/bird_1.png 	new file:   assets/birdy/bird_2.png 	new file:   assets/birdy/bird_3.png 	new file:   assets/birdy/bird_4.png 	new file:   assets/birdy/bird_5.png 	new file:   assets/birdy/bird_6.png 	new file:   assets/birdy/shad_bird_1.png 	new file:   assets/birdy/shad_bird_2.png 	new file:   assets/birdy/shad_bird_3.png 	new file:   assets/tile_images.png 	new file:   assets/tile_images.xcf 	new file:   birdy.spr.png 	new file:   level.c 	new file:   main.c 	new file:   simple_bg.c 	new file:   simple_bg.h 	new file:   simple_bg.tmx 	new file:   tmx.py

---
+++ simple_bg.c
@@ -0,0 +1,2 @@
+#include <stdint.h>
+#include "simple_bg.h"
---
+++ simple_bg.h
@@ -0,0 +1,8 @@
+#include <stdint.h>
+// tile ids
+// layers
+#define simple_bg_Capa de Patrones 1 0
+// max indices :  145
+#define simple_bg_header TMAP_HEADER(40,30,TSET_16,TMAP_U8)
+extern const uint16_t simple_bg_tset[]; // from assets/tile_images.png
+extern const uint8_t simple_bg_tmap[][40*30];
